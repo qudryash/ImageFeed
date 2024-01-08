dd7ad4d6c13449aa2ce1e3899b38d0b135b67c83
qudryash <qudryashovav@yandex.ru>
2024-01-08 09:20:24 +0700

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Kudryashov Andrey on 08.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
