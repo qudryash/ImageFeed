aac5b6d6498aa24d32b4083a87b49fb2f2b025c2
qudryash <qudryashovav@yandex.ru>
2024-01-12 00:54:03 +0700

    Спринт 8. Добавлено: - объявлена связь таблицы с dataSource и delegate - добавлено расширение для ImagesListViewController: UITableViewDelegate и UITableViewDataSource - добавлены методы в расширениях

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -14,7 +14,21 @@ class ImagesListViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+}
+    
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
