a95c1528f85d66a0bc44622080171e2804c26d63
qudryash <qudryashovav@yandex.ru>
2024-01-27 17:01:11 +0700

    Спринт 9. Выполнено: - добавлены соответствующие @IBOutlet'ы и @IBAction; - добавлена связь ViewControoler'а с кодом; - добавлена приватность к лейблам и к кнопке.

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Kudryashov Andrey on 27.01.2024.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet private var userPhotoImageView: UIImageView!
+    @IBOutlet private var userNameLabel: UILabel!
+    @IBOutlet private var nickNameLabel: UILabel!
+    @IBOutlet private var aboutMeLabel: UILabel!
+    @IBOutlet private var logoutButton: UIButton!
+    
+    @IBAction private func didTapLogoutButton() {
+    }
+
+}
