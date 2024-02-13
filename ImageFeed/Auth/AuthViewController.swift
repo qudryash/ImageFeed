3d2f03332e72107aa84e5779eae979492c679b4f
qudryash <qudryashovav@yandex.ru>
2024-02-13 16:31:42 +0700

    Спринт 10. Выполнено:
    - Добавлен новый экран контроллера авторизации;
    - Сверстан экран авторизации согласно макета:
    	- добавлена иконка;
    	- добавлена кнопка Войти;
    	- добавлены констрейнты;
    	- настроены цвета;
    	- выставлен радиус для кнопки.
    - Создан в проекте файл AuthViewController.swift и в нём объявлен класс AuthViewController;
    - В Main задан для View Controller'а класс AuthViewController;
    - В Main встроен AuthViewController в Navigation Controller.

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Kudryashov Andrey on 13.02.2024.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+}
