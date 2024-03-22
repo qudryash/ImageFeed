d0d1005b78e2f1edf6b3145b82b575ab7e49b5bd
qudryash <qudryashovav@yandex.ru>
2024-03-23 01:59:01 +0700

    Спринт 11. Устраняем гонку в проекте. Выполнено: - Добавлена обработка для потенциальной гонки в ImageFeed

--- ImageFeed/OAuth/OAuth2Service.swift
+++ ImageFeed/OAuth/OAuth2Service.swift
@@ -11,6 +11,10 @@ class OAuth2Service {
     
     static let shared = OAuth2Service()
     private let urlSession = URLSession.shared
+    
+    private var task: URLSessionTask?
+    private var lastCode: String?
+    
     private (set) var authToken: String? {
         get {
             return OAuth2TokenStorage().token
@@ -21,21 +25,33 @@ class OAuth2Service {
     }
     
     func fetchOAuthToken(_ code: String, completion: @escaping (Result<String, Error>) -> Void) {
+        
+        assert(Thread.isMainThread)
+        if lastCode == code {
+            return
+        }
+        task?.cancel()
+        lastCode = code
+        
         let request = authTokenRequest(code: code)
         let task = object(for: request) { [weak self] result in
-            guard let self = self else { return }
             DispatchQueue.main.async {
+                guard let self = self else {
+                    return
+                }
                 switch result {
                 case .success(let body):
                     let authToken = body.accessToken
                     self.authToken = authToken
                     completion(.success(authToken))
+                    self.task = nil
                 case .failure(let error):
                     completion(.failure(error))
+                    self.lastCode = nil
                 }
             }
         }
-        
+        self.task = task
         task.resume()
     }
 }
@@ -48,7 +64,9 @@ extension OAuth2Service {
         let decoder = JSONDecoder()
         return urlSession.data(for: request) { (result: Result<Data, Error>) in
             let response = result.flatMap { data -> Result<OAuthTokenResponseBody, Error> in
-                Result { try decoder.decode(OAuthTokenResponseBody.self, from: data) }
+                Result {
+                     try decoder.decode(OAuthTokenResponseBody.self, from: data)
+                 }
             }
             completion(response)
         }
